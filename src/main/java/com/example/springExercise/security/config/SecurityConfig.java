00ab815f02b8e4b251d1d902402328fa30c89937
hyun6ik <fkanwm@naver.com>
2021-04-30 22:47:07 +0900

    Form Login 인증

---
+++ src/main/java/com/example/springExercise/security/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.springExercise.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http //인가정책
+                .authorizeRequests()   // 요청에 대한 인가 확인
+                .anyRequest().authenticated();   //어떠한 요청에도 인가함
+
+        http //인증정책
+                .formLogin()               //폼 로그인 방식으로 인증
+//                .loginPage("/loginPage")    // 누구나 접근 가능해야함
+                .defaultSuccessUrl("/")     // 성공했을때 redirect되는 url
+                .failureUrl("/login")       // 실패했을때 redirect되는 url
+                .usernameParameter("userId")  // ID 파라미터 설정 (html상에서)
+                .passwordParameter("passwd")  // PWD 파라미터 설정    (html상에서)
+                .loginProcessingUrl("/login_proc")  // 로그인 Form action URL
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication = " + authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                })                                  // 로그인 성공시 생성되는 핸들러
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                                        AuthenticationException exception) throws IOException, ServletException {
+                        System.out.println("exception = " + exception.getMessage());
+                        response.sendRedirect("/login");
+                    }
+                })                                  // 로그인 실패시 생성되는 핸들러
+                .permitAll();                       // 로그인 페이지는 인증을 받기위해서 만들어진 페이지니까 인증이 없어도 허용 가능
+    }
+}
