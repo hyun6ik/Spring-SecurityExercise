0b3546c673132df9f2bb66f6b7cc141e472ee245
hyun6ik <fkanwm@naver.com>
2021-04-30 23:36:09 +0900

    로그아웃 처리

--- src/main/java/com/example/springExercise/security/config/SecurityConfig.java
+++ src/main/java/com/example/springExercise/security/config/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -49,5 +52,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })                                  // 로그인 실패시 생성되는 핸들러
                 .permitAll();                       // 로그인 페이지는 인증을 받기위해서 만들어진 페이지니까 인증이 없어도 허용 가능
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me");
     }
 }
