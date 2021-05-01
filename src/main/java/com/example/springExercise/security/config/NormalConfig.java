8ad85eee55bd7be9310a2ac51a83ccd1a09b4a01
DESKTOP-NQ23D5Q\Hyunsik <fkanwm@phpschool.com>
2021-05-01 19:11:31 +0900

    사이트 간 요청 위조 - CSRF, CsrfFilter

---
+++ src/main/java/com/example/springExercise/security/config/NormalConfig.java
@@ -0,0 +1,24 @@
+package com.example.springExercise.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class NormalConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+
+        http
+                .formLogin();
+
+//        http
+//                .csrf().disable();
+    }
+}
