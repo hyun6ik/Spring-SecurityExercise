e32161b7a20141f0e1547df320f5725c4a02ed9d
DESKTOP-NQ23D5Q\Hyunsik <fkanwm@phpschool.com>
2021-05-01 15:28:47 +0900

    동시 세션 제어, 세션 고정 보호, 세션 정책

--- src/main/java/com/example/springExercise/security/config/SecurityConfig.java
+++ src/main/java/com/example/springExercise/security/config/SecurityConfig.java
@@ -35,54 +35,61 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();   //어떠한 요청에도 인가함
 
         http //인증정책
-                .formLogin()               //폼 로그인 방식으로 인증
+                .formLogin();         //폼 로그인 방식으로 인증
 //                .loginPage("/loginPage")    // 누구나 접근 가능해야함
-                .defaultSuccessUrl("/")     // 성공했을때 redirect되는 url
-                .failureUrl("/login")       // 실패했을때 redirect되는 url
-                .usernameParameter("userId")  // ID 파라미터 설정 (html상에서)
-                .passwordParameter("passwd")  // PWD 파라미터 설정    (html상에서)
-                .loginProcessingUrl("/login_proc")  // 로그인 Form action URL
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                                        Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication = " + authentication.getName());
-                        response.sendRedirect("/");
-                    }
-                })                                  // 로그인 성공시 생성되는 핸들러
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-                                                        AuthenticationException exception) throws IOException, ServletException {
-                        System.out.println("exception = " + exception.getMessage());
-                        response.sendRedirect("/login");
-                    }
-                })                                  // 로그인 실패시 생성되는 핸들러
-                .permitAll();                       // 로그인 페이지는 인증을 받기위해서 만들어진 페이지니까 인증이 없어도 허용 가능
+//                .defaultSuccessUrl("/")     // 성공했을때 redirect되는 url
+//                .failureUrl("/login")       // 실패했을때 redirect되는 url
+//                .usernameParameter("userId")  // ID 파라미터 설정 (html상에서)
+//                .passwordParameter("passwd")  // PWD 파라미터 설정    (html상에서)
+//                .loginProcessingUrl("/login_proc")  // 로그인 Form action URL
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+//                                                        Authentication authentication) throws IOException, ServletException {
+//                        System.out.println("authentication = " + authentication.getName());
+//                        response.sendRedirect("/");
+//                    }
+//                })                                  // 로그인 성공시 생성되는 핸들러
+//                .failureHandler(new AuthenticationFailureHandler() {
+//                    @Override
+//                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+//                                                        AuthenticationException exception) throws IOException, ServletException {
+//                        System.out.println("exception = " + exception.getMessage());
+//                        response.sendRedirect("/login");
+//                    }
+//                })                                  // 로그인 실패시 생성되는 핸들러
+//                .permitAll();                       // 로그인 페이지는 인증을 받기위해서 만들어진 페이지니까 인증이 없어도 허용 가능
+//
+//        http
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/login")
+//                .addLogoutHandler(new LogoutHandler() {
+//                    @Override
+//                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+//                        HttpSession session = request.getSession();
+//                        session.invalidate();
+//                    }
+//                })
+//                .logoutSuccessHandler(new LogoutSuccessHandler() {
+//                    @Override
+//                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        response.sendRedirect("/login");
+//                    }
+//                })
+//                .and()
+//                .rememberMe()
+//                .rememberMeParameter("remember")
+//                .tokenValiditySeconds(3600)
+//                .userDetailsService(userDetailsService);
 
-        http
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler(new LogoutHandler() {
-                    @Override
-                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                        HttpSession session = request.getSession();
-                        session.invalidate();
-                    }
-                })
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.sendRedirect("/login");
-                    }
-                })
-                .and()
-                .rememberMe()
-                .rememberMeParameter("remember")
-                .tokenValiditySeconds(3600)
-                .userDetailsService(userDetailsService);
-
+//        http    // 동시 세션 제어
+//                .sessionManagement()
+//                .maximumSessions(1)
+//                .maxSessionsPreventsLogin(false);
+        http    // 세션 고정 보호
+                .sessionManagement()
+                .sessionFixation().none();
 
     }
 }
